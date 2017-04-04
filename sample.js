7302c62a21bbfbb7e596b7f57e9103cfb5263591
Levi Durfee <levi.durfee@gmail.com>
2017-04-04 17:36:02 -0400

    move words file

--- sample.js
+++ sample.js
@@ -1,5 +1,5 @@
 const Wennsshein = require("./src/index.js");
 
 var w = new Wennsshein()
-w.populate('./src/words.txt'); // get list of words
+w.populate('words.txt'); // get list of words
 w.solve('1234');
