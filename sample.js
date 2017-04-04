9ea830cc5ab3166d5cbaeffa9ea8769161bd7ac0
Levi Durfee <levi.durfee@gmail.com>
2017-04-04 17:43:06 -0400

    add a few more dictionary files

--- sample.js
+++ sample.js
@@ -1,5 +1,5 @@
 const Wennsshein = require("./src/index.js");
 
 var w = new Wennsshein()
-w.populate('words.txt'); // get list of words
-w.solve('1234');
+w.populate('./dictionaries/10k.txt'); // get list of words
+w.solve('12');
