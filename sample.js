c8059c0402676742ddec114be89dc4ae6100e0bc
Levi Durfee <levi.durfee@gmail.com>
2017-04-04 17:45:14 -0400

    break up 1234 into two groups
    this will demonstrate how you can find smaller words

--- sample.js
+++ sample.js
@@ -3,3 +3,6 @@ const Wennsshein = require("./src/index.js");
 var w = new Wennsshein()
 w.populate('./dictionaries/10k.txt'); // get list of words
 w.solve('12');
+w.solve('34');
+
+// Possible for 1234: teen mario
